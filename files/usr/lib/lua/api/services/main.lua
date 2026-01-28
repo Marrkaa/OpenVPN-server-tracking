b6a4ed3c35a9eff2b763bed7527b5d0a7e1c4d9a
Martynas <martynas.dmitrovskis@teltonika.lt>
2026-01-28 16:00:00 +0200

    Removed unneeded comments

--- files/usr/lib/lua/api/services/main.lua
+++ files/usr/lib/lua/api/services/main.lua
@@ -1,12 +1,8 @@
 #!/usr/bin/lua
--- main.lua - OpenVPN ubus daemon entry point
-
 local openvpn_ubus = require("openvpn_ubus")
 
--- Konfigūracija (galima skaityti iš config failo vėliau)
-local SERVER_NAME = "myserver"  -- Pakeisti į realų serverio pavadinimą
+local SERVER_NAME = "myserver"
 local MGMT_HOST = "127.0.0.1"
 local MGMT_PORT = 7505
 
--- Paleisti ubus service
 openvpn_ubus.run(SERVER_NAME, MGMT_HOST, MGMT_PORT)
